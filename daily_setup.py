4b191ab25c2a2b4b5fcb0579a0337c7328888dcf
Pamela Wu <pamela.wu@shopify.com>
2022-12-01 08:15:22 -0500

    space for two solutions in setup script

--- daily_setup.py
+++ daily_setup.py
@@ -19,20 +19,26 @@ def fetch_input_data(year, day):
 
 def make_solution_file(year, day):
     solution_template = f"""
-def solution(data):
-    return "answer goes here"
+def solution_the_first(data):
+    return "first answer goes here"
+
+
+def solution_the_second(data):
+    return "second answer goes here"
 
 
 if __name__ == "__main__":
     with open(f"data/{year}/{day:02d}.txt", "r") as f:
         data = f.read()
 
-    print(solution(data))"""
+    print(solution_the_first(data))
+    print(solution_the_second(data))"""
 
-    filename = f"solutions/{year}/{day:02d}.txt"
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(f"solutions/{year}/{day:02d}.py", "w") as f:
-        f.write(solution_template)
+    filename = f"solutions/{year}/{day:02d}.py"
+    if not os.path.exists(filename):
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, "w") as f:
+            f.write(solution_template)
     return
 
 
