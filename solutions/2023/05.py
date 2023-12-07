e8f44e36c5cf70d688df90e47955b27ca8dfd8b8
Pamela Wu <pamela.wu@shopify.com>
2023-12-06 23:40:24 -0500

    2023 day 5 part 2

--- solutions/2023/05.py
+++ solutions/2023/05.py
@@ -11,16 +11,17 @@ def parse_input(data):
         map_values = [list(map(int, m.split(" "))) for m in map_raw[1:] if m]
         map_values.sort(key=lambda x: x[1])
 
-        store = {"source": list(), "target": list()}
+        store = {"source": list(), "target": list(), "delta": list()}
         for target, source, length in map_values:
             store["source"].append((source, source + length - 1)) # inclusive
             store["target"].append((target, target + length - 1))
+            store["delta"].append(target - source)
 
         maps[map_type] = store
     return seeds, maps
 
 
-def traverse_maps(seed, maps, start_type="seed", end_type="location"):
+def seed_to_location(seed, maps, start_type="seed", end_type="location"):
     map_types = maps.keys()
 
     map_type = [m for m in map_types if m[0] == start_type][0]
@@ -44,55 +45,86 @@ def traverse_maps(seed, maps, start_type="seed", end_type="location"):
     return to_value
 
 
+def interval_overlap(source, rseed):
+    """
+        (s1, e1)
+        (s2, e2)
+    """
+    s1, e1, = source
+    s2, e2 = rseed
+    if e2 < s1 or s2 > e1: # none
+        return None
+    elif s2 >= s1 and e2 <= e1: # full inner
+        return [(s2, e2)]
+    elif s2 >= s1 and s2 <= e1 and e2 > e1: # right
+        return [(s2, e1), (e1 + 1, e2)]
+    elif s2 < s1 and s1 <= e2 and e1 >= e2: # left
+        return [(s2, s1 - 1), (s1, e2)]
+    elif s2 < s1 and e2 > e1: # full outer
+        return [(s2, s1 - 1), (s1, e1), (e1 + 1, e2)]
+    else:
+        raise Exception(source, rseed, "match problem")
+
+
+def seed_ranges_to_location(rseeds, maps):
+    stages = [("seed", "soil"), ("soil", "fertilizer"), ("fertilizer", "water"),
+            ("water", "light"), ("light", "temperature"), ("temperature", "humidity"),
+            ("humidity", "location")]
+
+    while stages:
+        stage = stages.pop(0)
+
+        # check all intervals, split if necessary
+        while True:
+            new_sources = []
+            for rseed in rseeds:
+                for source in maps[stage]["source"]:
+                    overlaps = interval_overlap(source, rseed)
+                    if overlaps:
+                        new_sources += overlaps
+                if all(not interval_overlap(s, rseed) for s in maps[stage]["source"]):
+                    new_sources.append(rseed)
+            new_sources = list(set(new_sources))
+            if len(new_sources) == len(rseeds):
+                break
+            else:
+                rseeds = new_sources
+
+        # split intervals should only match once now or not at all
+        targets = []
+        for ns in new_sources:
+            for source, d in zip(maps[stage]["source"], maps[stage]["delta"]):
+                overlaps = interval_overlap(source, ns)
+                assert overlaps == None or len(overlaps) == 1
+                if overlaps:
+                    ov = overlaps[0]
+                    targets.append((ov[0] + d, ov[1] + d))
+            if all(not interval_overlap(s, ns) for s in maps[stage]["source"]):
+                targets.append(ns)
+
+        rseeds = targets
+
+    return targets
+
+
 def first_star(data):
     seeds, maps = parse_input(data)
     locations = []
     for seed in seeds:
-        locations.append(traverse_maps(seed, maps))
+        locations.append(seed_to_location(seed, maps))
     return min(locations)
 
 
 def second_star(data):
-    return "second answer goes here"
+    seeds, maps = parse_input(data)
+    rseeds = [(s, s + l - 1) for s, l in zip(seeds[0::2], seeds[1::2])]
+    location_ranges = seed_ranges_to_location(rseeds, maps)
+    return sorted(location_ranges, key=lambda t: t[0])[0][0]
 
 
 if __name__ == "__main__":
     with open(f"data/2023/05.txt", "r") as f:
         data = f.read()
-
-    #     data = """seeds: 79 14 55 13
-
-    # seed-to-soil map:
-    # 50 98 2
-    # 52 50 48
-
-    # soil-to-fertilizer map:
-    # 0 15 37
-    # 37 52 2
-    # 39 0 15
-
-    # fertilizer-to-water map:
-    # 49 53 8
-    # 0 11 42
-    # 42 0 7
-    # 57 7 4
-
-    # water-to-light map:
-    # 88 18 7
-    # 18 25 70
-
-    # light-to-temperature map:
-    # 45 77 23
-    # 81 45 19
-    # 68 64 13
-
-    # temperature-to-humidity map:
-    # 0 69 1
-    # 1 0 69
-
-    # humidity-to-location map:
-    # 60 56 37
-    # 56 93 4"""
 
     print(first_star(data))
     print(second_star(data))
